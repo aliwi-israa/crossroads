d5be72225917073225c493fddac30e1645dba37f
aliwi-israa <israaaliwi@gmail.com>
2025-06-06 03:05:37 +0300

    popup gray overlay

--- includes/popup.php
+++ includes/popup.php
@@ -1,6 +1,7 @@
+<div class="popup-overlay" id="popupOverlay"></div>
 <div class="popup" id="promoPopup">
     <div class="row">
-        <div class="col-7">
+        <div class="col-7 img-section">
             <img src="images/popup.jpg" alt="Dental Promo Image" class="promo-img">
         </div>
         <div class="col-5 text-section">
@@ -10,11 +11,12 @@
             <h3 class="mt-4 mb-5">Welcome to Your New Dental Home</h3>
             <p class="my-3">
                 Take the first step toward a healthier smile with our $99 New Patient Special.
-                We’re here to make your visit easy, affordable, and all about you.
+                <br/>
+                <span class="mt-1">We’re here to make your visit easy, affordable, and all about you.<span>
             </p>
               <div class="col-lg-12 mt-5 text-center">
                 <a class="btn-secondary fx-slide btn-rounded" href="services.html"><span>Book Appointment Today</span></a>
-                <p>or Call (905) 334-2113</p>
+                <p>or Call<a href="tel:(+1) 123-456-789">(+1) 123-456-789</a></p>
                 </div>
             </div>
         </div>
--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
     setTimeout(function () {
         document.getElementById("promoPopup").style.display = "block";
+        document.getElementById("popupOverlay").style.display = "block";
     }, 2000);
 
-    // Close popup when clicking the close button
     document.querySelector(".close-btn").addEventListener("click", function () {
         closePopup();
     });
 
-    // Close popup when pressing Escape key
     document.addEventListener("keydown", function (event) {
         if (event.key === "Escape") {
             closePopup();
@@ -18,4 +17,5 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function closePopup() {
     document.getElementById("promoPopup").style.display = "none";
+    document.getElementById("popupOverlay").style.display = "none";
 }
